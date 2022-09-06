3049ab284ead0d3f5b4f804f613602d1c174ed4b
Lucia cataldo <lcataldo.lc@gmail.com>
2022-09-06 16:29:41 -0300

    Ejercicio Clase 3

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Ejercicio 1
+
+for (let i = 1; i <= 12; i++){
+    let nombreUsuario = prompt ("Hola! Ingrese su Nombre");
+if(i === 10){
+    mensaje = alert (` ${nombreUsuario} Lo sentimos la clase ya esta completa`);
+    break;
+}
+let mensaje = `Bienvenido/a ${nombreUsuario}, eres el inscripto N° ${i}`;
+alert (mensaje);
+
+}
+
+// Elercicio 2
+
